723a58d891ebf18c925255a60b354dd7957b8e75
datptit16 <nqdat160602@gmail.com>
2022-02-14 11:22:59 +0700

    update commit

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -69,54 +69,29 @@
 // 
 // 
 
-let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
-let theme = sessionStorage.getItem('theme');
 
-function prefersColorTest(systemInitiatedDark) {
-  if (systemInitiatedDark.matches) {
-	  document.documentElement.setAttribute('data-theme', 'dark');
-	   // document.getElementById("theme-toggle").innerHTML = "Light Mode";
-   	sessionStorage.setItem('theme', '');
-  } else {
-	  document.documentElement.setAttribute('data-theme', 'light');
-    // document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-    sessionStorage.setItem('theme', '');
-  }
+
+
+
+
+
+
+// datk default
+(()=> {	modeSwitcher()})();
+
+function modeSwitcher() {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	sessionStorage.setItem('theme', 'dark');
 }
-systemInitiatedDark.addListener(prefersColorTest);
 
-
-// function modeSwitcher() {
-// 	let theme = sessionStorage.getItem('theme');
-// 	if (theme === "dark") {
+// 	if (currentTheme === "dark") {
 // 		document.documentElement.setAttribute('data-theme', 'light');
 // 		sessionStorage.setItem('theme', 'light');
-// 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-// 	}	else if (theme === "light") {
+// 	}	else if (currentTheme === "light") {
 // 		document.documentElement.setAttribute('data-theme', 'dark');
 // 		sessionStorage.setItem('theme', 'dark');
-// 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
-// 	} else if (systemInitiatedDark.matches) {
-// 		document.documentElement.setAttribute('data-theme', 'light');
-// 		sessionStorage.setItem('theme', 'light');
-// 		//let theme = sessionStorage.getItem('theme');
-// 		//console.log("this was triggered");
-// 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-// 	} else {
+// 	}else{
 // 		document.documentElement.setAttribute('data-theme', 'dark');
 // 		sessionStorage.setItem('theme', 'dark');
-// 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
 // 	}
-// }
-
-// if (theme === "dark") {
-// 	document.documentElement.setAttribute('data-theme', 'dark');
-// 	sessionStorage.setItem('theme', 'dark');
-// 	document.getElementById("theme-toggle").checked = false;
-// 	// document.getElementById("theme-toggle").innerHTML = "Light Mode";
-// } else if (theme === "light") {
-// 	document.documentElement.setAttribute('data-theme', 'light');
-// 	sessionStorage.setItem('theme', 'light');
-// 	document.getElementById("theme-toggle").checked = true;
-// 	// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
 // }
